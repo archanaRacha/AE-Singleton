a629f15155f4f19cfb0392f9ff27ab843a42c6db
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-02-06 21:22:27 +0530

    Singleton

--- Singleton/ViewController.swift
+++ Singleton/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct LoggedInUser {}
+struct FeedItem {}
+struct Follower {}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+// Main Module
+extension ApiClient{
+    func login(completion:(LoggedInUser) -> Void){}
+}
+extension ApiClient{
+    func loadFollower(completion: ([Follower]) -> Void){}
+}
+extension ApiClient{
+    func loadFeed(completion: ([FeedItem]) -> Void){}
 }
 
+// API Module
+//Singleton
+ class ApiClient {
+     static let instance = ApiClient()
+     private init(){}
+     func execute(_ : URLRequest, completion: (Data) -> Void){}
+     
+}
+
+class MockApiClient : ApiClient{}
+//singleton : there is convinience methodURLSession() and URLSession.shared
+
+
+// login Module
+class LoginViewController : UIViewController {
+    var login: (((LoggedInUser) -> Void) -> Void)?
+
+    func didTapLoginButton() {
+        login? { user in
+            //show next screen
+            
+        }
+    }
+}
+
+// Feed Module
+class FeedService {
+    var loadFeed: ((([FeedItem]) -> Void) -> Void)?
+     func load() {
+        loadFeed? { loadedItems in
+                // update UI
+            
+        }
+    }
+}
+
+// Follower Module
+class FollowerViewController : UIViewController {
+    var loadFollower: ((([Follower]) -> Void) -> Void)?
+    override func viewDidLoad() {
+        loadFollower? { followers in
+                // update UI
+            
+        }
+    }
+}
